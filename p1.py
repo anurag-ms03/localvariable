ed775dfef8eaaa8423b20ae43a3877f6074c738f
anurag-ms03 <anuragmsthefirst@gmail.com>
2023-11-18 21:55:13 +0530

    testing

--- p1.py
+++ p1.py
@@ -1,6 +1,5 @@
 input_string = "This is a sample string. This string is a sample. Sample is the keyword." 
 
-word_to_count = "sample" 
-count = input_string.lower().count(word_to_count.lower()) 
+
 
 print(f"The word '{word_to_count}' appears {count} times in the input string.") 
